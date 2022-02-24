412834ee9c0f675eab997398948ca7898b0b1157
niharbansal02 <niharbansal02@gmail.com>
2022-02-25 01:09:45 +0530

    Basic algo

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ from cv2 import split
 import numpy as np
 
 contributors = {}
+avail = {}
 skills = {}
 projects = {}
-
-
-'''
-Anna -> C++ 8
-    Python 9
-
-Python -> Anna 9
-'''
 skillsTotal = set()
+numProj = 0
+final_proj = {}
 
 def process_data(file_path):
     fh = open(file_path)
@@ -28,6 +23,7 @@ def process_data(file_path):
         l2 = fh.readline().strip().split()
         name, numSkill = l2[0], l2[1]
 
+        avail[name] = 0
         # contributors[name] = {
         #     "C++":9,
         #     "Python":8
@@ -46,15 +42,6 @@ def process_data(file_path):
     # ! Skills
     for name in contributors:
         name_obj = contributors[name]
-        # pprint(name_obj)
-
-        # skills = {
-        #     "C++" :{
-        #         "Maria":0
-        #     }
-        #     "Maria 0",
-        #     "Python": "Bob 5"
-        # }
 
         for skill in skillsTotal:
             if skill in name_obj:
@@ -93,18 +80,69 @@ def process_data(file_path):
     # print(skillsTotal, indent = 4)
     # pprint(skills, indent=4)
     # print(projects)
-    pprint(projects, indent=4)
+    # pprint(projects, indent=4)
     # print(c, p)
 
     fh.close()
 
-def write_data(data, file_path="out.txt"):
+def basic_algo():
+    for project_name in projects:
+        project = projects[project_name]
+        roles = project["roles"]
+
+        # TODO: Shortlist based on
+
+        all_avail = True
+        contri_allot = []
+
+        for role in roles:
+            levelReq = roles[role]
+            skill_avail = False
+
+            print(role, levelReq)
+
+            for contri_name in skills[role]:
+                skill_level = skills[role][contri_name]
+                
+                print(contri_name, role, skill_level)
+                
+                if avail[contri_name] == 0 and skill_level >= levelReq:
+                    contri_allot.append(contri_name)
+                    skill_avail = True
+
+            if skill_avail == False:
+                print(skill_avail)
+                all_avail == False
+                break
+        
+        if(all_avail == False):
+            print(project_name)
+            continue
+
+        # numProj = numProj + 1
+        final_proj[project_name] = contri_allot
+
+        for contri in contri_allot:
+            avail[contri] = 1
+
+
+def submission(file_path="out.txt"):
     oh = open(file_path, 'w')
-    oh.write(data)
+
+    pprint(final_proj)
+    # oh.writelines(len(final_proj))
+
+    # for proj_name in final_proj:
+    #     oh.writelines(proj_name)
+    #     oh.writelines(" ".join(final_proj[proj_name]))
+
     oh.close()
 
 if __name__ == '__main__':
     process_data("a_an_example.in.txt")
+    basic_algo()
+    submission("a.txt")
+    # pprint(avail)
     # process_data("b_better_start_small.in.txt")
     # process_data("c_collaboration.in.txt")
     # process_data("d_dense_schedule.in.txt")
